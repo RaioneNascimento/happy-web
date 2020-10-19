ce78bd8af8730110e2d4f756fc9485eda23930e4
raione <raione_bonfim@hotmail.com>
2020-10-19 20:19:13 -0300

    #4 dia, configuraçao backend

---
+++ src/database/fakedata.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-23.6244446",
+    lng: "-46.5649414",
+    name: "Lar das meninas",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1598454444314-28649fcaa0e9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/photo-1600712243809-7a3dd4e68fff?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/flagged/photo-1576042854593-1f6eebfcfb0c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    intrunctions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 08h até 18h",
+    open_on_weekends: "1"
+  },
+  {
+    id: 2,
+    lat: "-23.6244446",
+    lng: "-46.5549414",
+    name: "Lar dos meninos",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1598454444314-28649fcaa0e9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/photo-1600712243809-7a3dd4e68fff?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/flagged/photo-1576042854593-1f6eebfcfb0c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    intrunctions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 08h até 18h",
+    open_on_weekends: "0"   
+  }
+]
---
+++ src/server.js
@@ -0,0 +1,23 @@
+// Importar dependencias
+const express = require('express');
+const path = require('path')
+const pages = require('./pages.js')
+
+// Iniciando o Express 
+const server = express();
+server
+  // utilizando os arquivos estáticos
+  .use(express.static('public'))
+
+  //configurar template engine
+  .set('views', path.join(__dirname, "views"))
+  .set('view engine', 'hbs')
+
+  // criando rota 
+  .get('/', pages.index)
+  .get('/orphanage', pages.orphanage)
+  .get('/orphanages', pages.orphanages)
+  .get('/create-orphanage', pages.createOrphanage)
+  
+// Ligar o server
+server.listen(5500); 
